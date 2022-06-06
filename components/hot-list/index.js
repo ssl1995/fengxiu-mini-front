d963efeec5c18b5456702c291784efd476adc060
songshenglin <872912679@qq.com>
2022-06-06 22:06:48 +0800

    第三周 1-9 flex的灵活布局

--- components/hot-list/index.js
+++ components/hot-list/index.js
@@ -9,10 +9,12 @@ Component({
 
     /*新式监听器*/
     observers: {
+        // 监听上面定义的banner属性
         'banner': function (banner) {
             if (!banner || banner.items.length === 0) {
                 return;
             }
+            // 通过items里面数组的名字来监听
             const left = banner.items.find(t => t.name === 'left');
             const rightTop = banner.items.find(t => t.name === 'right-top');
             const rightBottom = banner.items.find(t => t.name === 'right-bottom');
