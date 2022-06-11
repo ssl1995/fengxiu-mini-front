57546be2c33d6f0134d675b39948b06a2d13a8ae
songshenglin <872912679@qq.com>
2022-06-11 17:00:32 +0800

    第三周3-5 瀑布流初步形成

--- components/spu-preview/index.js
+++ components/spu-preview/index.js
@@ -1,23 +1,31 @@
 // components/spu-preview/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    data: Object
-  },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        data: Object
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    data: {
+        tags: Array
+    },
 
-  },
+    observers: {
+        data: function (data) {
+            if (!data || !data.tags) {
+                return
+            }
+            const tags = data.tags.split('$')
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+            this.setData({
+                tags
+            })
+        }
+    },
 
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
 })
