15ef00f999c06abc36f6b21ad7b670a75086b311
songshenglin <872912679@qq.com>
2022-06-13 22:44:15 +0800

    第5周 1-3 Cell去重

--- components/models/cell.js
+++ components/models/cell.js
@@ -1,8 +1,11 @@
 class Cell {
+    // 规格值
     title
+    id
 
     constructor(spu) {
         this.title = spu.value;
+        this.id = spu.value_id;
     }
 }
 
--- components/models/fence.js
+++ components/models/fence.js
@@ -3,25 +3,46 @@ import { Cell } from "./cell";
 class Fence {
     cells = []
     specs
+    // 规格名
+    title
+    id
 
     constructor(specs) {
         this.specs = specs;
+        this.title = specs.key;
+        this.id = specs.key_id
     }
 
+
+
     init() {
+        this._initCells();
+    }
+
+    _initCells() {
         this.specs.forEach(s => {
-            // this.pushValueTitles(s.value);
 
             const cell = new Cell(s);
+            // some:是如果有，就立马break返回
+            // every:是全部遍历
+            const equals = this.cells.some(c => {
+                if (c.id === s.value_id) {
+                    return true;
+                }
+            })
+
+            if (equals) {
+                return;
+            }
 
             this.cells.push(cell);
         });
     }
 
 
-    pushValueTitles(title) {
-        this.valueTitles.push(title)
-    }
+    // pushValueTitles(title) {
+    //     this.valueTitles.push(title)
+    // }
 
 }
 
